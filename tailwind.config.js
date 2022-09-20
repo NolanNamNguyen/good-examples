1e40b9901d1d44dcd7985a81832ed5ae5cd4acc1
Nam Nguyen Van <namvannguyen.andy@gmail.com>
2022-09-20 23:12:27 +0700

    Add custom plugin config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import("tailwindcss").Config} */
+const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -10,9 +11,45 @@ module.exports = {
         'primary-font': '#000000',
         'loading-background-color': '#FFFFFF59',
       },
+      fontSize: {
+        'etp-title-1': [
+          '24px',
+          {
+            letterSpacing: '0.05em',
+            lineHeight: 'calc(29 / 24)',
+          },
+        ],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities }) {
+      matchUtilities(
+        {
+          // Class name
+          'mui-field-span': (value) => {
+            return {
+              '& .Mui-focused': {
+                '& fieldset': {
+                  '& span': {
+                    width: `${value}`,
+                  },
+                },
+              },
+            };
+          },
+          'mui-field-label': (value) => {
+            return {
+              '& label': {
+                fontSize: `${value}`,
+              },
+            };
+          },
+        },
+        null,
+      );
+    }),
+  ],
   corePlugins: {
     preflight: false,
   },
